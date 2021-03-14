105996a5656180e792c984de588ea7c9c44bd52d
boo <booblegum42@gmail.com>
2021-03-13 16:43:13 -0800

    Create, delete and view list snippets

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from datetime import datetime
 
+LANG_CHOICE = [
+    ("py", "python"),
+    ("cpp", "C++"),
+    ("js", "JavaScript"),
+]
 
 class Snippet(models.Model):
     name = models.CharField(max_length=100)
-    lang = models.CharField(max_length=30)
+    lang = models.CharField(max_length=30, choices=LANG_CHOICE)
     code = models.TextField(max_length=5000)
-    creation_date = models.DateTimeField()
+    creation_date = models.DateTimeField(default=datetime.now())
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,23 +1,66 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
+from MainApp.forms import SnippetForm
+from MainApp.models import Snippet
 
 
 def get_base_context(request, pagename):
-    return {
-        'pagename': pagename
-    }
+    return
 
 
-def index_page(request):
-    context = get_base_context(request, 'PythonBin')
+def index(request):
+    context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
 
-def add_snippet_page(request):
-    context = get_base_context(request, 'Добавление нового сниппета')
-    return render(request, 'pages/add_snippet.html', context)
+def snippet(request, snippet_id):
+    context = {'pagename': 'Страница сниппета'}
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+        # comments = snippet.comment_set.all()
+    except Snippet.DoesNotExist:
+        raise Http404
+
+    context["snippet"] = snippet
+    # context["comments"] = comments
+    return render(request, 'pages/snippet.html', context)
 
 
-def snippets_page(request):
-    context = get_base_context(request, 'Просмотр сниппетов')
+def snippet_list(request):
+    context = {'pagename': 'Просмотр сниппетов'}
+    snippets = Snippet.objects.all()
+    context["snippets"] = snippets
     return render(request, 'pages/view_snippets.html', context)
+
+
+def snippet_add(request):
+    # Если GET-запрос,то мы хотим получить сраницу с формой создания сниппета
+    context = {'pagename': 'Добавление нового сниппета'}
+    if request.method == "GET":
+        form = SnippetForm()
+        context["form"] = form
+        return render(request, 'pages/add_snippet.html', context)
+    # Если POST-запрос,то мы получаем его от формы "создание сниппета"
+    elif request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            snippet = form.save(commit=False)
+            if request.user.is_authenticated:
+                snippet.user = request.user
+            snippet.save()
+            return redirect('snippet_list')
+        # Если данные не валидные, возвращаем страницу с формой и отображаем ошибки заполнения
+        form = SnippetForm(request.POST)
+        print("errors = ", form.errors)
+        context["form"] = form
+        return render(request, 'pages/add_snippet.html', context)
+
+
+def snippet_delete(request, snippet_id):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except Snippet.DoesNotExist:
+        raise Http404
+
+    snippet.delete()
+    return redirect('snippet_list')
